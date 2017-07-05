63774b1af86007b0acd0c2f17af04b2cc8441a28
xuchao <85825770@qq.com>
2017-07-05 18:46:52 +0800

    加载兼容脚本，似乎没个卵用

---
+++ Es5Asset.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace xutl\tcplayer;
+
+use yii\web\View;
+
+/**
+ * Asset bundle for TcPlayAsset Widget
+ */
+class Es5Asset extends \yii\web\AssetBundle
+{
+
+
+    public $js = [
+        '//imgcache.qq.com/open/qcloud/video/vcplayer/libs/es5-shim.js',
+        '//imgcache.qq.com/open/qcloud/video/vcplayer/libs/es5-sham.js'
+    ];
+
+    public $jsOptions = ['condition' => 'lte IE9'];
+}
--- TcPlayAsset.php
+++ TcPlayAsset.php
@@ -15,4 +15,8 @@ class TcPlayAsset extends \yii\web\AssetBundle
     public $js = [
         '//imgcache.qq.com/open/qcloud/video/vcplayer/TcPlayer-2.2.0.js',
     ];
+
+//    public $depends = [
+//        'xutl\tcplayer\Es5Asset',
+//    ];
 }
